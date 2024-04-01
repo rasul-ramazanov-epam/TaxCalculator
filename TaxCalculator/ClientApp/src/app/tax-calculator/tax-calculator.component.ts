53f864dd542b0d7165931ca77e18fd5d6535bfe3
Rasul Ramazanov <Rasul_Ramazanov@epam.com>
2024-04-01 10:42:52 +0300

    Add component for tax calculation

---
+++ TaxCalculator/ClientApp/src/app/tax-calculator/tax-calculator.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface TaxCalculationResult {
+  grossAnnualSalary: number;
+  netAnnualSalary: number;
+  annualTaxPaid: number;
+}
+
+@Component({
+  selector: 'app-tax-calculator',
+  templateUrl: './tax-calculator.component.html',
+  styleUrls: ['./tax-calculator.component.css']
+})
+export class TaxCalculatorComponent {
+  grossSalary: number = 0;
+  taxCalculationResult: TaxCalculationResult = {
+    grossAnnualSalary: 0,
+    netAnnualSalary: 0,
+    annualTaxPaid: 0,
+  };
+
+  constructor(private http: HttpClient) { }
+
+  calculateTax(): void {
+    const url = 'api/taxcalculator/calculate';
+    const requestBody = { grossSalary: this.grossSalary };
+
+    this.http.post<TaxCalculationResult>(url, requestBody).subscribe(result => {
+      this.taxCalculationResult = result;
+    });
+  }
+
+  get netSalary(): number {
+    return this.taxCalculationResult.netAnnualSalary;
+  }
+
+  get annualTaxPaid(): number {
+    return this.taxCalculationResult.annualTaxPaid;
+  }
+}
